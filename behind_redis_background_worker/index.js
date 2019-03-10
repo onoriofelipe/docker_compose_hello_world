f82a0c202a12ba9f99255c6b6b62a0d5672954a7
onoriofelipe_at_fedoratoucan <onoriofelipe7@gmail.com>
2019-03-09 23:01:30 -0300

    Node async semantics review and calculating fib
    Implemented three workflows for dealing with node async syntax/semantics
    including dealing with errors: completely lost, using promises, and
    using async/await keywords. After revisiting promise documentation it
    started to really sink in how promises are a relatively simple and very
    elegant solution for a relatively complex problem. The api is stupidly
    simple for what it does. Bonus is finding out that a .finally(...)
    method has been implemented in node v10 and reasonably recent browser
    versions. Another bonus is the promisify api. So simple yet so useful.
    Should find out at some point is the 'util' is a npm package, a node
    library, or standard javascript by now.
    
    Implemented recursive and non-recursive fibonacci number calculators.
    Node chokes somewhere between 40 and 50 but computer keeps going on just
    fine due to node using a single thread in a multi-core processor.
    
    Started implementing the skeleton of pub/sub event logic. Probably
    syntax is all mixed but haven't tested yet.

--- behind_redis_background_worker/index.js
+++ behind_redis_background_worker/index.js
@@ -4,13 +4,16 @@ const {promisify} = require('util'); // promisify, no npm modules required?
 // or has this already been included somewhere else? test later
 
 const port = '4000';
-const redisClient = redisModule.createClient();
-const redisGetAsync = promisify(redisClient.get).bind(redisClient);
+const redisSubscriberClient = redisModule.createClient();
+const redisPublisherClient = redisModule.createClient();
+const redisGetAsync = promisify(redisSubscriberClient.get).bind(redisSubscriberClient);
 const app = express();
 
 var globalCount = 0;
 var anotherGlobalCount = 0;
 var otherGlobalCount = 0;
+
+// ASYNC REVIEW PART
 
 // everything is confusing and amorph at this point, however there's probably
 // a relatively tidy way to clean this up and make it work by putting the 
@@ -18,10 +21,10 @@ var otherGlobalCount = 0;
 // including one await keywork)
 app.get('/first_try', (req, res) => {
     let fullResponse = 'initial response string<br>';
-    redisClient.set('some_counter', (++globalCount).toString());
+    redisSubscriberClient.set('some_counter', (++globalCount).toString());
     let someAsyncFunc = async (rs,rj) => {
         let somePromise = new Promise( (resolve, reject) => {
-            redisClient.get('some_counter', (err, result) => {
+            redisSubscriberClient.get('some_counter', (err, result) => {
                 if(err) console.log(err.stack)
                 else {
                     console.log('Redis results:');
@@ -41,7 +44,7 @@ app.get('/first_try', (req, res) => {
 // (some time later...)
 app.get('/promises', (req, res) => {
     let fullResponse = 'initial response string<br>';
-    redisClient.set('some_other_counter', (++anotherGlobalCount).toString());
+    redisSubscriberClient.set('some_other_counter', (++anotherGlobalCount).toString());
     // alternatively it becomes similar to the async/await flow,
     // but with one additional callback:
     // let somePromise = redisGetAsync(...);
@@ -60,7 +63,7 @@ app.get('/promises', (req, res) => {
 
 app.get('/await', async (req, res) => {
     let fullResponse = 'initial response string<br>';
-    redisClient.set('some_other_counter', (++otherGlobalCount).toString());
+    redisSubscriberClient.set('some_other_counter', (++otherGlobalCount).toString());
     try {
         let result = await redisGetAsync('some_other_counter');
         // everything until the end of the block is equivalent to a then block
@@ -80,5 +83,61 @@ app.get('/await', async (req, res) => {
     // f().catch((err)=>{...});
 });
 
+// default routes for miscellaneous testing
+// they are down here so the url parameter doesn't gobble up the previous routes
+// since express parses the routes sequentially
+// getting a url parameter:
+// requires express, different workflow from node's default way
+// url.parse(req.url, true).query
+app.get('/:number', (req, res) => {
+    let numberString = req.params.number;
+    let number = parseInt(numberString);
+    let resultString = `Calculating fibonacci number sequentially<br>n: ${number.toString()}<br>fib(n): ${nonRecursiveFib(number)}`;
+    console.log(resultString);
+    res.send(resultString);
+});
+
+app.get('/recursive/:number', (req, res) => {
+    let numberString = req.params.number;
+    let number = parseInt(numberString);
+    let resultString = `Calculating fibonacci number recursivelly<br>n: ${number.toString()}<br>fib(n): ${recursiveFib(number)}`;
+    console.log(resultString);
+    res.send(resultString);
+});
+
+
+// PUB AND SUB PART
+// note: no calls to routes are being made, communication between one express
+// instance and another is happening through redis via its sub and pub events
+/*
+//[]TODO: change to saving data on a dictionary instead of just publishing it
+redisSubscriberClient.sub('fib_channel');
+redisSubscriberClient.on('message', 'fib_channel', (stringValue) => {
+    desiredValue = parseInt(stringValue);
+    calculatedValue = nonRecursiveFib(desiredValue);
+    // calculatedValue = recursiveFib(desiredValue);
+    redisPublisherClient.pub('fib_channel', calculatedValue.toString());
+});
+*/
+function recursiveFib(n){
+    if(n == 0 || n == 1){
+        return 1;
+    }
+    return recursiveFib(n-1) + recursiveFib(n-2);
+}
+
+function nonRecursiveFib(n){
+    let a = 0;
+    let b = 1;
+    for( i = 0; i <= n; ++i ){
+        b = a+b;
+        a = b-a;
+    }
+    return b;
+}
+
+// n   0   1   2   3   4   5   6
+// r   1   1   2   3   5   8   13
+// c   1   1   1+1 1+2 2+3 3+5 5+8
 
 app.listen(port, (err) => {if(err) console.log(`Express error: ${err}`);});
